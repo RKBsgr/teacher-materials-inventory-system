975455c852020fc542291ac623c616262e46d3b8
RKBsgr <keanu.basagre2004@gmail.com>
2026-03-19 20:10:14 +0800

    Fix CORS for deployment

--- server/index.js
+++ server/index.js
@@ -11,7 +11,9 @@ const PORT = process.env.PORT || 5000;
 
 /* ===================== MIDDLEWARE ===================== */
 
-app.use(cors());
+app.use(cors({
+  origin: "https://teacher-materials-inventory.vercel.app"
+}));
 app.use(express.json());
 
 // Serve uploaded files
