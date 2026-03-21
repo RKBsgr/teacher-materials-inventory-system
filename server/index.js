cafb5dc26d28d5fe18e6c876a424d00922889707
RKBsgr <keanu.basagre2004@gmail.com>
2026-03-21 17:34:40 +0800

    Saved

--- server/index.js
+++ server/index.js
@@ -9,6 +9,16 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+//added
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 /* ===================== MIDDLEWARE ===================== */
 
 app.use(cors({
@@ -17,14 +27,15 @@ app.use(cors({
 app.use(express.json());
 
 // Serve uploaded files
-app.use("/uploads", express.static("uploads"));
+//app.use("/uploads", express.static("uploads"));
 
 /* ===================== FILE UPLOAD ===================== */
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "materials",
+    resource_type: "auto" // supports pdf, docx, images, etc
   }
 });
 
@@ -300,7 +311,7 @@ app.post(
       let filePath;
 
       if (req.file) {
-        filePath = `/uploads/${req.file.filename}`;
+        filePath = req.file.path; // Cloudinary URL
       } else if (req.body.url) {
         filePath = req.body.url;
       } else {
